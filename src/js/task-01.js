9e7308c7e8a0ef635f330c5507a639105c388f22
Дмитро Авраменко <105928143+Dmytro-Avramenko@users.noreply.github.com>
2022-10-09 00:34:32 +0300

    first task finish ))

---
+++ src/js/task-01.js
@@ -0,0 +1,23 @@
+const categoriesEl = document.querySelectorAll(".item");
+console.log('Number of categories:', categoriesEl.length);
+
+const categoriesRef = document.querySelector('#categories');
+
+const AnimalsEl = categoriesRef.children[0];
+const ProductsEl = categoriesRef.children[1];
+const TechnologiesEl = categoriesRef.children[2];
+
+const AnimalsCat = AnimalsEl.children[0].textContent;
+const AnimalsElem = AnimalsEl.children[1].children.length;
+console.log('Category:', AnimalsCat);
+console.log('Elements:', AnimalsElem);
+
+const ProductsCat = ProductsEl.children[0].textContent;
+const ProductsElem = ProductsEl.children[1].children.length;  
+console.log('Category:', ProductsCat);
+console.log('Elements:', ProductsElem);
+
+const TechnologiesCat = TechnologiesEl.children[0].textContent;
+const TechnologiesElem = TechnologiesEl.children[1].children.length; 
+console.log('Category:', TechnologiesCat);
+console.log('Elements:', TechnologiesElem);
