dc206a2894c8982f1ff0180446f021a3ecfee909
Дмитро Авраменко <105928143+Dmytro-Avramenko@users.noreply.github.com>
2022-10-17 21:52:31 +0300

    third task finish

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,55 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+// Перший спосіб працює)
+// const firstImgEl = document.createElement('img');
+// firstImgEl.src = 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+// firstImgEl.alt = 'White and Black Long Fur Cat';
+// firstImgEl.width = 320;
+
+// const liFirstImgEl = document.createElement('li');
+// liFirstImgEl.appendChild(firstImgEl);  
+
+
+// const secondImgEl = document.createElement('img');
+// secondImgEl.src = 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'; 
+// secondImgEl.alt = 'Orange and White Koi Fish Near Yellow Koi Fish';
+// secondImgEl.width = 320;
+
+// const liSecondImgEl = document.createElement('li');
+// liSecondImgEl.appendChild(secondImgEl);
+
+
+// const thirdImgEl = document.createElement('img');
+// thirdImgEl.src = 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+// thirdImgEl.alt = 'Group of Horses Running';
+// thirdImgEl.width = 320;
+
+// const liThirdImgEl = document.createElement('li');
+// liThirdImgEl.appendChild(thirdImgEl);
+
+// const galleryRef = document.querySelector('ul');
+// galleryRef.append(liFirstImgEl, liSecondImgEl, liThirdImgEl);
+// console.log(galleryRef);
+
+
+// Другий спосіб)
+const galleryEl = document.querySelector('.gallery');
+
+const makeGallery = items => {
+  return items.map(
+    item => {
+    const galleryItem = document.createElement('li');
+    
+    galleryItem.insertAdjacentHTML('beforeend',
+    `<img src=${item.url} 
+    alt=${item.alt} 
+    width="320">`);
+
+    return galleryItem;
+  });
+};
+
+const elements = makeGallery(images);
+galleryEl.append(...elements);
