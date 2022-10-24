b7cf3a4742985593a029cfde96fdf15d7c647937
Дмитро Авраменко <105928143+Dmytro-Avramenko@users.noreply.github.com>
2022-10-24 22:52:10 +0300

    finish 9 task

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refBody = document.querySelector('body');
+const refButton = document.querySelector('.change-color');
+const textValue = document.querySelector('.color');
+
+refButton.addEventListener('click', onTargetButtonClick);
+
+function onTargetButtonClick() {
+  refBody.style.backgroundColor = getRandomHexColor();
+  textValue.textContent = getRandomHexColor();
+
+  // console.log(refBody.style.backgroundColor)
+  // console.log(textValue.textContent)
+  // console.log(`клік`)
+}
